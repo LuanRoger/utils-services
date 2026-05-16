d1f134424729653faebc686174e694ff3dd0141c
ROG <luan.roger.2003@gmail.com>
2026-05-16 09:08:53 -0300

    feat: add common plugins and update some errors response

--- apps/todo/src/index.ts
+++ apps/todo/src/index.ts
@@ -1,15 +1,45 @@
+import bearer from "@elysia/bearer";
+import cors from "@elysia/cors";
+import serverTiming from "@elysia/server-timing";
 import { Elysia } from "elysia";
+import logixlysia from "logixlysia";
 import { ENV } from "varlock/env";
-import { TodoModule } from "./modules/todo";
+import todoRoutes from "./modules/todo";
 
 const port = ENV.PORT || 8081;
 const hostname = ENV.HOST || "0.0.0.0";
 
 const app = new Elysia()
-  .use(TodoModule)
-  .get("/", () => "OK")
-  .listen({ port, hostname });
+  .use(
+    logixlysia({
+      config: {
+        service: "@utils/todo",
+        showStartupMessage: true,
+        startupMessageFormat: "simple",
+        showContextTree: true,
+        contextDepth: 2,
+        slowThreshold: 150,
+        verySlowThreshold: 500,
+        ip: false,
+      },
+    })
+  )
+  .use(
+    cors({
+      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS"],
+    })
+  )
+  .use(serverTiming())
+  .use(bearer())
+  .onBeforeHandle(({ status, bearer }) => {
+    const apiKey = ENV.API_KEY;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+    if (bearer !== apiKey) {
+      return status("Unauthorized");
+    }
+  })
+  .use(todoRoutes)
+  .get("/", () => "OK");
+
+app.listen({ port, hostname });
