7664417b5ab21882f4b56800d6dc2a2595d2f200
ROG <luan.roger.2003@gmail.com>
2026-05-16 08:55:32 -0300

    feat(stock): add common plugins

--- apps/stock/src/index.ts
+++ apps/stock/src/index.ts
@@ -1,18 +1,46 @@
+import bearer from "@elysia/bearer";
+import cors from "@elysia/cors";
+import serverTiming from "@elysia/server-timing";
 import Elysia from "elysia";
+import logixlysia from "logixlysia";
 import { ENV } from "varlock/env";
-import FiagroRoutes from "./modules/fiagro/routes";
-import FiisRoutes from "./modules/fiis/routes";
+import fiagroRoutes from "./modules/fiagro/routes";
+import fiisRoutes from "./modules/fiis/routes";
 
 const hostname = ENV.HOST || "0.0.0.0";
 const port = ENV.PORT || 8082;
 
-const app = new Elysia();
+const app = new Elysia()
+  .use(
+    logixlysia({
+      config: {
+        service: "@utils/stock",
+        showStartupMessage: true,
+        startupMessageFormat: "simple",
+        showContextTree: true,
+        contextDepth: 2,
+        slowThreshold: 150,
+        verySlowThreshold: 500,
+        ip: false,
+      },
+    })
+  )
+  .use(
+    cors({
+      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ["GET", "OPTIONS"],
+    })
+  )
+  .use(serverTiming())
+  .use(bearer())
+  .onBeforeHandle(({ status, bearer }) => {
+    const apiKey = ENV.API_KEY;
 
-app.use(FiisRoutes);
-app.use(FiagroRoutes);
+    if (bearer !== apiKey) {
+      return status("Unauthorized");
+    }
+  })
+  .use(fiisRoutes)
+  .use(fiagroRoutes);
 
 app.listen({ hostname, port });
-
-console.log(
-  `🦊 Server is running at ${app.server?.hostname}:${app.server?.port}`
-);
